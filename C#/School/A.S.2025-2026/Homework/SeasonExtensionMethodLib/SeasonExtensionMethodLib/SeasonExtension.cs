b40123f4f4c2eea5262d07d994c1f43d6ee032b9
Cesare-Di-Masi <cesare.dimasi.stud@ispascalcomandini.it>
2025-10-06 15:01:34 +0200

    completed season extension

--- C#/School/A.S.2025-2026/Homework/SeasonExtensionMethodLib/SeasonExtensionMethodLib/SeasonExtension.cs
+++ C#/School/A.S.2025-2026/Homework/SeasonExtensionMethodLib/SeasonExtensionMethodLib/SeasonExtension.cs
@@ -8,38 +8,86 @@ namespace SeasonExtensionMethodLib
 {
     public static class SeasonExtension
     {
-        public static Seasons GetSeason(this DateTime currDate)
+        public static Seasons GetSeason(this DateTime date)
         {
-            if (currDate.Year % 4 == 0)
-            {
-                if(currDate.DayOfYear >= 81 && currDate.DayOfYear <= 172)
+            // Estrae giorno e mese per un confronto più semplice
+            int day = date.Day;
+            int month = date.Month;
 
-                if (currDate.DayOfYear >= 173 && currDate.DayOfYear <= 263)
-                {
-                    return Seasons.SUMMER;
-                }
+            if (month == 3 && day >= 21 || month > 3 && month < 6 || month == 6 && day < 21)
+                return Seasons.SPRING;
 
-                if(currDate.DayOfYear >= 264 && currDate.DayOfYear<=354)
-                {
-                    return Seasons.FALL;
-                }
-            }
-            else
-            {
-                if (currDate.DayOfYear >= 172 && currDate.DayOfYear <= 263)
-                {
-                    return Seasons.SUMMER;
-                }
-            }
+            if (month == 6 && day >= 21 || month > 6 && month < 9 || month == 9 && day < 22)
+                return Seasons.SUMMER;
+
+            if (month == 9 && day >= 22 || month > 9 && month < 12 || month == 12 && day < 21)
+                return Seasons.FALL;
+
+            // Deve essere inverno
+            return Seasons.WINTER;
         }
 
         public static bool IsSummer(this DateTime currDate)
         {
-            
+            if (currDate.GetSeason() == Seasons.SUMMER)
+            {
+                return true;
+            }
+
+            return false;
         }
 
+        public static int DaysForNextSeason(this DateTime currDate)
+        {
+            // 1. Ottiene la stagione corrente
+            Seasons currentSeason = currDate.GetSeason();
 
+            // 2. Determina la data di inizio della PROSSIMA stagione
+            DateTime nextSeasonStartDate;
 
+            switch (currentSeason)
+            {
+                case Seasons.SPRING:
+                    // La prossima è l'Estate (21 Giugno dello stesso anno)
+                    nextSeasonStartDate = new DateTime(currDate.Year, 6, 21);
+                    break;
 
+                case Seasons.SUMMER:
+                    // La prossima è l'Autunno (22 Settembre dello stesso anno)
+                    nextSeasonStartDate = new DateTime(currDate.Year, 9, 22);
+                    break;
+
+                case Seasons.FALL:
+                    // La prossima è l'Inverno (21 Dicembre dello stesso anno)
+                    nextSeasonStartDate = new DateTime(currDate.Year, 12, 21);
+                    break;
+
+                case Seasons.WINTER:
+                    // La prossima è la Primavera (21 Marzo dell'ANNO SUCCESSIVO)
+                    nextSeasonStartDate = new DateTime(currDate.Year + 1, 3, 21);
+                    break;
+
+                default:
+                    // Caso teoricamente irraggiungibile, ma buono per sicurezza
+                    throw new ArgumentOutOfRangeException("Stagione non valida.");
+            }
+
+            TimeSpan difference = nextSeasonStartDate - currDate;
+            int days = (int)difference.Days;
+
+            if (days > 365)
+            {
+                if (DateTime.IsLeapYear(currDate.Year))
+                {
+                    days -= 366;
+                }
+                else
+                {
+                    days -= 365;
+                }
+            }
+
+            return days;
+        }
     }
 }
---
+++ C#/School/A.S.2025-2026/Homework/SeasonExtensionMethodLib/TestSeasonExtMethod/TestSeasonExtMethods.cs
@@ -0,0 +1,38 @@
+﻿using SeasonExtensionMethodLib;
+
+namespace TestSeasonExtMethod
+{
+    [TestClass]
+    public class TestSeasonExtMethods
+    {
+        [TestMethod]
+        public void GetSeason_IsCorrect()
+        {
+            DateTime spring = new DateTime(2000, 3, 22);
+            DateTime summer = new DateTime(2000, 6, 22);
+            DateTime fall = new DateTime(2000, 9, 23);
+            DateTime winter = new DateTime(2000, 12, 22);
+
+            Assert.AreEqual(Seasons.SPRING, spring.GetSeason());
+            Assert.AreEqual(Seasons.SUMMER, summer.GetSeason());
+            Assert.AreEqual(Seasons.FALL, fall.GetSeason());
+            Assert.AreEqual(Seasons.WINTER, winter.GetSeason());
+        }
+
+        [TestMethod]
+        public void IsSummer_IsCorrect()
+        {
+            DateTime summer = new DateTime(2000, 6, 21);
+            DateTime notSummer = new DateTime(2000, 3, 20);
+            Assert.IsTrue(summer.IsSummer());
+            Assert.IsFalse(notSummer.IsSummer());
+        }
+
+        [TestMethod]
+        public void DaysForNextSeason_IsCorrect()
+        {
+            DateTime date = new DateTime(2000, 1, 1);
+            Assert.AreEqual(79, date.DaysForNextSeason());
+        }
+    }
+}
