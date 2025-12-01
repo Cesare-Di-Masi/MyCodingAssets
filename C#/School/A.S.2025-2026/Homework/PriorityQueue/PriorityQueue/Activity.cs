637ea9aaaa206e2cbb69705131c02324f085152b
cesare.dimasi <cesare.dimasi@PC-5105>
2025-12-01 08:59:43 +0100

    added priorityQueue

---
+++ C#/School/A.S.2025-2026/Homework/PriorityQueue/PriorityQueue/Activity.cs
@@ -0,0 +1,36 @@
+﻿namespace PriorityQueue
+{
+    public class Activity:IComparable<Activity>
+    {
+        private string _id;
+        private int _priority, _timeToElaborate;
+
+        public string ID
+        {
+            get { return _id; }
+        }
+
+        public int Priority
+        {
+            get { return _priority; }
+        }
+
+        public int TimeToElaborate
+        {
+            get { return _timeToElaborate; }
+        }
+
+        public Activity(string id, int prority, int timeToElaborate)
+        {
+            _id = id;
+            _priority = prority;
+            _timeToElaborate = timeToElaborate;
+        }
+
+        public int CompareTo(Activity? other)
+        {
+            if (other == null) return 1;
+            return this.Priority.CompareTo(other.Priority);
+        }
+    }
+}
